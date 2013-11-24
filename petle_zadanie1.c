98b9f1b77001272b9265485875b3c94cd6651371
kdabrowski <biz@op.pl>
2013-11-24 09:21:27 +0100

    zadanie 1

---
+++ petle_zadanie1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+int liczba, wynik;
+liczba = 1;
+while (wynik <= 2010)
+		{
+			
+			wynik = liczba*2;
+			printf("%d \n", wynik);
+			liczba = liczba *2;
+		}
+return 0;
+
+}

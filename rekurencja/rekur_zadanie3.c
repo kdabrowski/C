eab5c3d25c1c07459c6cf42f507be9cf54378186
kdabrowski <biz@op.pl>
2013-12-12 01:28:34 +0100

    rekurencje

--- rekurencja/rekur_zadanie3.c
+++ rekurencja/rekur_zadanie3.c
@@ -1,11 +1,27 @@
 #include<stdio.h>
 
+char odwroc(char a[]);
+
 int main(void){
     int i, y;
-    char *c, a[10];
-    c = a[]
+    char c[10];
     printf("podaj napis: ");
-        scanf("%s", c);
-        printf("%s", c);
-    
+        fgets(c, 10, stdin);
+        odwroc(c);
+        printf("%s\n", c);
+   return 0;
+}
+
+char  odwroc(char a[]){
+    int i, y;
+    char wynik[10];
+    y = 0;
+    i = 10;
+    while (i >= 0){
+    a[i] = wynik[y];
+    --i;
+    ++y;
+    }
+
+    return wynik[10];
 }
---
+++ rekurencja/rekur_zadanie4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int euklides(int a, int b);
+
+int main (void){
+    int x,y,c;
+
+    printf("Podaj pierwsza liczbe ");
+    scanf("%d", x);
+    printf("Podaj pierwsza liczbe ");
+    scanf("%d", y);
+    c = euklides(x,y);
+    printf("Najmniejszy wspolny dzielnik to ", c);
+}
+
+int euklides(int a, int b){
+    temp
+}
